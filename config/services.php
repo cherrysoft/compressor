6c2ffc5c0a00130d1089a897908267042c92221a
Michael Quinn <michael.quinn@videoelephant.com>
2018-10-23 15:00:07 +0200

    initial config with services

---
+++ config/services.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => Compressor\Models\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+    'encoder' => [
+        'default' => env('DEFAULT_ENCODER_SERVICE')
+    ],
+
+    'telestream' => [
+        'api_key'    => env('TELESTREAM_API_KEY'),
+        'factory_id' => env('TELESTREAM_FACTORY_ID'),
+    ],
+
+    'coconut' => [
+        'api_key' => env('COCONUT_API_KEY')
+    ]
+
+];
